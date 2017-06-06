7a9a2f7a83aefe28c6fc75820951369cb9060f28
wasp <michakirsch@gmx.net>
2017-06-06 20:53:31 +0200

    Add swagger and sensor endpoint

--- api/controllers/SensorController.js
+++ api/controllers/SensorController.js
@@ -21,11 +21,11 @@ var util = require('util');
   - The HTTP Verb of the corresponding operation (get, put, post, delete, etc)
   - Or the operationId associated with the operation in your Swagger document
 
-  In the starter/skeleton project the 'get' operation on the '/hello' path has an operationId named 'hello'.  Here,
-  we specify that in the exports of this module that 'hello' maps to the function named 'hello'
+  In the starter/skeleton project the 'get' operation on the '/sensor' path has an operationId named 'sensor'.  Here,
+  we specify that in the exports of this module that 'sensor' maps to the function named 'sensor'
  */
 module.exports = {
-  hello: hello
+  sensor: getSensorData
 };
 
 /*
@@ -34,11 +34,11 @@ module.exports = {
   Param 1: a handle to the request object
   Param 2: a handle to the response object
  */
-function hello(req, res) {
+function getSensorData(req, res) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var name = req.swagger.params.name.value || 'stranger';
-  var hello = util.format('Hello, %s!', name);
+  var value = util.format('42');
+    //ToDo: Write sensor logic here
 
   // this sends back a JSON response which is a single string
-  res.json(hello);
+  res.json(value);
 }
--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 'use strict';
 
-var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
+
+const SwaggerExpress = require('swagger-express-mw');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const app = require('express')();
 module.exports = app; // for testing
 
 var config = {
   appRoot: __dirname // required config
 };
+
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
@@ -17,7 +22,7 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   var port = process.env.PORT || 10010;
   app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+//  if (swaggerExpress.runner.swagger.paths['/sensor']) {
+//    console.log('try this:\ncurl http://127.0.0.1:' + port + '/sensor?name=Scott');
+//  }
 });
